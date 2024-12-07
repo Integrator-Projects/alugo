fffc6ec79a48a0b79968a68b8e876dc3ed1bbe5c
KarlaNunes <karla.julyana1@gmail.com>
2024-12-07 17:17:29 -0300

    chore: initial setup

---
+++ src/main/java/com/ifrn/domusmanager/DomusManagerApplication.java
@@ -0,0 +1,13 @@
+package com.ifrn.domusmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomusManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DomusManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ifrn/domusmanager/DomusManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifrn.domusmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomusManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
