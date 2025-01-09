380f22d24c6ed8c24108b31f140542788ab88d11
KarlaNunes <karla.julyana1@gmail.com>
2025-01-08 21:06:51 -0300

    test: list all apartments

--- src/test/java/com/ifrn/alugo/service/ApartmentServiceTest.java
+++ src/test/java/com/ifrn/alugo/service/ApartmentServiceTest.java
@@ -12,7 +12,12 @@ import org.junit.jupiter.api.*;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -66,6 +71,23 @@ class ApartmentServiceTest {
         verify(apartmentMapper).toEntity(apartmentRequestDTO);
         verify(apartmentRepository).save(apartment);
         verify(apartmentMapper).toResponseDTO(apartment);
+    }
+
+    @Test
+    @DisplayName("Lista todos os apartamentos")
+    public void testListApartments_ShouldListApartmentsSuccessfully() {
+        Apartment apartment1 = Instancio.create(Apartment.class);
+
+        List<Apartment> apartments = List.of(apartment1);
+        Pageable pageable = PageRequest.of(0, 10);
+        Page<Apartment> apartmentPage = new PageImpl<>(apartments, pageable, apartments.size());
+        ApartmentResponseDTO apartmentResponseDTO = Instancio.create(ApartmentResponseDTO.class);
+
+        when(apartmentRepository.findAll(pageable)).thenReturn(apartmentPage);
+        when(apartmentMapper.toResponseDTO(apartment1)).thenReturn(apartmentResponseDTO);
+
+        Page<ApartmentResponseDTO> result = apartmentService.getAllApartments(pageable);
+        assertEquals(2, result.getContent().size());
     }
 
 }
