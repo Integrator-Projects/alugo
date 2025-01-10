d79530997bb916a11d86b99c164b4224b0b77e20
KarlaNunes <karla.julyana1@gmail.com>
2025-01-10 18:59:04 -0300

    feat: add address validations

--- src/main/java/com/ifrn/alugo/entity/Address.java
+++ src/main/java/com/ifrn/alugo/entity/Address.java
@@ -1,6 +1,10 @@
 package com.ifrn.alugo.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,10 +19,22 @@ public class Address {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "A cidade não pode ser vazia")
     private String city;
+
+    @NotBlank(message = "Estado não pode ser vazio")
     private String state;
+
+    @Pattern(regexp = "\\d{5}-\\d{3}", message = "Formato de CEP inválido")
     private String zipCode;
+
+    @NotBlank(message = "A rua não pode ser vazia")
     private String street;
+
+    @NotBlank(message = "O bairro não pode ser vazio")
     private String neighborhood;
+
+    @NotNull(message = "Número não pode ser nulo")
+    @Positive(message = "Número deve ser maior que zero")
     private Integer number;
 }
