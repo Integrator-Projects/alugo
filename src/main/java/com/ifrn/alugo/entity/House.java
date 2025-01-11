d5f1dad30f5ffc64db3dd9830dae09d60b611e46
KarlaNunes <karla.julyana1@gmail.com>
2025-01-10 22:57:41 -0300

    feat: add validations in House

--- src/main/java/com/ifrn/alugo/entity/House.java
+++ src/main/java/com/ifrn/alugo/entity/House.java
@@ -1,6 +1,8 @@
 package com.ifrn.alugo.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,23 +18,28 @@ public class House {
     private Long id;
 
     @Column(name = "rental_price")
+    @PositiveOrZero(message = "Valor do aluguel deve ser positivo")
     private Double rentalPrice;
 
     private String description;
 
-    private Boolean available;
+    private Boolean available = true;
 
     @Column(name = "number_of_bathrooms")
+
+    @Positive(message = "Número de banheiros deve ser maior que zero")
     private Integer numberOfBathrooms;
 
     @Column(name = "number_of_bedrooms")
+    @Positive(message = "Número de quartos deve ser maior que zero")
     private Integer numberOfBedrooms;
 
     @Column(name = "area_in_m2")
+    @Positive(message = "Área deve ser maior que zero")
     private Double areaInM2;
 
     @Column(name = "has_garage")
-    private Boolean hasGarage;
+    private Boolean hasGarage = false;
 
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "address_id", referencedColumnName = "id")
